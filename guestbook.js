427f0c8e94d633c7f7bb6cbd1741c1aa759fb964
Valentyn Hrabovskyi <34043225+valentin-grabovskiy@users.noreply.github.com>
2022-05-28 11:36:53 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b632359b.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
